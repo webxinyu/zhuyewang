f66556c3bd38655bfebf12b39f0ca16ac026780e
zhoufei <15003707121@163.com>
2017-12-08 13:22:27 +0800

    添加服务

--- zywweb/server/server.js
+++ zywweb/server/server.js
@@ -1,11 +1,21 @@
-var http = require('http');
-http.createServer(function(request, response) {
+var http = require("http");
+var url = require('url');
+
+function start(route){
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+	  	console.log("Request for " + pathname + "received.");
+	  	
+	  	route(pathname);
+	  	
+	  	response.writeHead(200, {"Content-Type": "text/plain"});
+	  	response.write("Hello World");
+	  	response.end();
+	}
 	
-	//发送http头部
-	//http状态值：200 ok
-	//内容类型：text/plain
-	response.writeHead(200,{'Content-Type':'text/plain'});
+	http.createServer(onRequest).listen(8888);
 	
-	//发送相应数据‘hello world’
-	response.end('hello world');
-})
+	console.log("Server has started.");	
+}
+
+exports.start = start;
